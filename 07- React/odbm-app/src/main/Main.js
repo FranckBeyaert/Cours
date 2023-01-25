8da4abdee8a22bd99cc6219a9b49975cd4204362
Franck <franck.beyaert59@gmail.fr>
2023-01-25 14:22:02 +0100

    Ajout Login

--- 07- React/odbm-app/src/main/Main.js
+++ 07- React/odbm-app/src/main/Main.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Main.css'
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
 import Preferences from '../components/Preferences';
+import App from '../App';
 import Login from '../components/Login';
+import useToken from '../components/useToken'
 
 
 function Main()
 {
-    const [token, setToken] = useState()
+    const { token, setToken } = useToken();
+
 
     if(!token)
     {
@@ -23,7 +26,7 @@ function Main()
                     <Routes>
                         <Route path="/dashboard" element={<Dashboard/>}/>
                         <Route path="/preferences" element={<Preferences/>}/>
-                    </Routes>
+                        <Route path="/app" element={<App/>}/>                    </Routes>
                 </BrowserRouter>
             </React.StrictMode>
         </div>
